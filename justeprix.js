834ace84ade1b0d6c18355c81445ef3cca9e9251
Jerry Frederic <jerry.fpl@gmail.com>
2021-01-10 08:24:07 +0100

    etape5 gestion envoie formulaire

--- justeprix.js
+++ justeprix.js
@@ -4,8 +4,8 @@ let error = document.querySelector('small') ;
 let formulaire = document.querySelector('#formulaire') ;
 
 // Etape 2 - Cacher l'erreur
-error.style.visibility = "hidden";
-//error.style.display = "none";
+//error.style.visibility = "hidden";
+error.style.display = "none";
 
 // Etape 3 - Générer un nombre aléatoire entre 0 et 1000
 let nombreAleatoire = Math.floor(Math.random() * 1001) ;
@@ -13,7 +13,15 @@ let nombreAleatoire = Math.floor(Math.random() * 1001) ;
 //Math.floor --> avoir uniquement des nbres entiers
 
 // Etape 4 - Vérifier que l'utilisateur donne bien un nombre
-
+input.addEventListener('keyup', () => {
+    if(isNaN(input.value)){
+        //affiche le message d'erreur
+        error.style.display = 'block';
+    }else{
+        //ne pas afficher message d'erreur
+        error.style.display = "none";
+    }
+})
 // Etape 5 - Agir à l'envoi du formulaire
 
 // Etape 6 - Créer la fonction vérifier
